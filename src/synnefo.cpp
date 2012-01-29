c19f05432579dfb6767a835517f934540e648d73
Joe Simon <jsimon383@gmail.com>
2012-01-28 21:29:03 -0800

    added BSD license header to base files

--- src/synnefo.cpp
+++ src/synnefo.cpp
@@ -1,3 +1,29 @@
+/*
+Copyright (C) 2011-2012 Joseph Simon <jsimon383@gmail.com>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions o9hband the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS ORbsd
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORTcd
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 #include "synnefo.h"
 
 
@@ -60,8 +86,6 @@ void Synnefo::changeModuleSelection(int rowIndex)
                            setText((moduleList.at(i) )->getDescription() );
             moduleNameLabel->setText( QString("Synnefo ") 
                                         + (moduleList.at(i) )->getName() );
-
-            break;
         }
     }  
 }
@@ -102,10 +126,11 @@ void Synnefo::closeSynnefo()
 
 void Synnefo::loadSyModules()
 /*  Pre: SyModule objects and module list are unintialized.
-    Post: Initializes each SyModule objects and populates the module list (moduleList).
-          The module selection in the main Synnefo dialog will be refreshed.  */
+    Post: Initializes each SyModule objects and populates the module listing.
+          The module selection in the main Synnefo dialog will be refreshed.*/
 {
-    // TODO Find solution to automatically check for # of modules in directory structure.    
+    // TODO Find solution to automatically check 
+    //      for # of modules in directory structure.    
     
     devicesModule = new SyDevices(0);       // "Synnefo Devices"
     infoModule = new SyInfo(0);             // "Synnefo Information"
--- src/synnefo.h
+++ src/synnefo.h
@@ -1,4 +1,28 @@
-//  "synnefo.h"
+/*
+Copyright (C) 2011-2012 Joseph Simon <jsimon383@gmail.com>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions o9hband the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS ORbsd
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORTcd
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 
 #ifndef SYNNEFO_H
 #define SYNNEFO_H
@@ -46,48 +70,38 @@ public:
     
     Synnefo (QWidget* parent);
     ~Synnefo();
-    
 
 private slots:    
     
-    void changeModuleSelection ( int );
-    
-    void closeSynnefo();
-    
-    void freeSyModules();
-    
-    void openApplicationSettings();
-    
-    void refreshModuleList();
-    
+    void changeModuleSelection ( int );    
+    void closeSynnefo();    
+    void freeSyModules();    
+    void openApplicationSettings();    
+    void refreshModuleList();   
     
 private:      
   
 //  *** Private Functions *********************    
     
-    void loadSyModules();
-    
+    void loadSyModules();    
     void saveState();
     
     
 //  *** Private Data Members ******************    
     
-    // List to store Synnefo modules.
-    QList <SyModule*> moduleList;
+    
+    QList <SyModule*> moduleList;       // List to store Synnefo modules.
     
     // Synnefo module widget interfaces.
-    SyDevices* devicesModule;    
-    SyInfo* infoModule;
+    SyDevices* devicesModule;          
+    SyInfo* infoModule;                
     SySettings* settingsModule;
       
-    // Synnefo config dialog.
-    SyConfig* configDialog;
+    SyConfig* configDialog;            // Synnefo config dialog.
     
-    // Flag to determine first run of the application.
-    bool isFirstRun;  
-        
-    // Variable to save Ui state.
-    QSettings configuration;
+    bool isFirstRun;                   // Flag to determine first run.
+    
+    QSettings configuration;           // Variable to save Ui state.
 };
 
 #endif
